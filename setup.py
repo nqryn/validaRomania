4f6e1dc49e9fda0f1d767b237074fe241d07e621
Adela Neacsu <adella.neacsu@gmail.com>
2017-10-09 12:31:18 +0300

    Some initial changes
    Commit for changing author for previous commit, because Git...

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="validari-romania",
+    version="0.0.1",
+    packages=["validari-romania",],
+    url="https://github.com/nqryn/validari-Romania",
+    license="MIT",
+    author="nqryn",
+    author_email="adella.neacsu@gmail.com",
+    description="Acest repo conține diverse validări pentru diverse componente specific românești ( CNP, numere de telefon, IBAN, numere de înmatriculare auto, ș.a.m.d. )",
+    requires=[]
+)
